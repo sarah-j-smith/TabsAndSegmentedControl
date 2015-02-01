677b5f00b57c2b9a593762508ea73194b9abd471
Sarah Smith <sarah@smithsoft.com.au>
2015-02-02 07:38:20 +1000

    Initial Commit

---
+++ TabsAndSegmentedControl/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  TabsAndSegmentedControl
+//
+//  Created by Sarah Smith on 2/02/2015.
+//  Copyright (c) 2015 Sarah Smith. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
